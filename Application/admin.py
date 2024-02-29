667dc913bb07326c2495dde0cc4d23c85980d2f9
NavandarM <133376879+NavandarM@users.noreply.github.com>
2024-02-29 16:29:21 +0100

    Update admin.py

--- Application/admin.py
+++ Application/admin.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
 from Application.models import UserOpinion, GeneralInfo
 
-# Register your models here.
-#PriBrainOrthoLnc:Evolution_2023
-#PrimeBrainLnc:
 
 admin.site.register(GeneralInfo)
 admin.site.register(UserOpinion)
-# admin.site.register(OrthoInfy)
 
