8d8d4859a63edcd75a4ac5a322e767497e1b1b59
NavandarM <mohitnavandar11@gmail.com>
2026-08-12 17:03:55 +0200

    Optimize views, clean up dead code, fix repo hygiene

--- Application/views.py
+++ Application/views.py
@@ -1,21 +1,23 @@
 from django.shortcuts import render
 from Application.models import GeneralInfo
 from .forms import ExplorationFormByIDs, UserMessageForm, ExplorationForm, ExploreFormSeq, ExploreMultipleIds
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from django.contrib import messages
 import re
-from django.db.models import Q
-from django.views.generic import View, TemplateView
+from functools import lru_cache
+from django.views.generic import TemplateView
 import pandas as pd
 import numpy as np
 import os
-from .utils import get_simple_plot
 from django.conf import settings
 import subprocess
 from django.urls import reverse
-from django.utils.safestring import mark_safe  # Remove if not used!
-from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 from html import unescape
+import plotly.express as px
+
+# Organism name -> UCSC genome browser assembly id, shared by every search path
+ORGANISM_ASSEMBLY = {"Human": "hg19", "Chimp": "panTro4", "Gorilla": "gorGor4", "Gibbon": "nomLeu3"}
 
 
 # View: Home Page
@@ -62,70 +64,64 @@ def userArea(request):
     return render(request, 'user_input.html',{'form':form, 'submitted':submitted})
 
 #### Function of splitting the string into a specific pattern
-def query_processor( query_entity, organism_entity ):
+def query_processor(query_entity, organism_entity):
 
-    if query_entity:
-        for field in query_entity:
-            LncID = field.LncRNA_id
-            #human_ortho = entity.Orthologs_Human.replace(';',' ;')
-            human_ortho = field.Orthologs_Human.replace(' ','').split(';')
-            #chimp_ortho = entity.Orthologs_Chimp.replace(';',' ;')
-            chimp_ortho = field.Orthologs_Chimp.replace(' ','').split(';')
-            #gorilla_ortho = entity.Orthologs_Gorilla.replace(';',' ;')
-            gorilla_ortho = field.Orthologs_Gorilla.replace(' ','').split(';')
-            #gibbon_ortho = entity.Orthologs_Gibbon.replace(';',' ;')
-            gibbon_ortho = field.Orthologs_Gibbon.replace(' ','').split(';')
+    entry = query_entity.first()
+    if entry is None:
+        return f"No entires found! Probably you entered invalid lncRNA id for {organism_entity}"
 
-            my_list = [ human_ortho, chimp_ortho, gorilla_ortho, gibbon_ortho, LncID ]
-            
-            return my_list
-    else:
-        WARN = "No entires found! Probably you entered invalid lncRNA id for " + organism_entity
-        return WARN
+    orthologs = [
+        getattr(entry, field).replace(' ', '').split(';')
+        for field in ('Orthologs_Human', 'Orthologs_Chimp', 'Orthologs_Gorilla', 'Orthologs_Gibbon')
+    ]
+    return orthologs + [entry.LncRNA_id]
 
 #### Preparation of the data for visualization
 
-def Data_preparation ( organism, lncRNA_Id ):
-    ## File path
-    human_file = os.path.join(settings.STATIC_DIR,'files/ExpressionCounts','Normalized_Read_Counts_Human.txt')
-    chimp_file = os.path.join(settings.STATIC_DIR,'files/ExpressionCounts', 'Normalized_Read_Counts_Chimp.txt')
-    gorilla_file = os.path.join(settings.STATIC_DIR,'files/ExpressionCounts','Normalized_Read_Counts_Gorilla.txt')
-    Organism_files = {'Human':human_file, 'Chimp': chimp_file, 'Gorilla': gorilla_file}
-    try:
-        if organism in Organism_files:
-            Exp_data = pd.read_csv(Organism_files[organism], sep="\t")      # Open the file of desired organism
-            Exp_data1 = Exp_data.set_index('Ids')                           # Assign Ids are row names
-            Entity = Exp_data1.loc[lncRNA_Id].to_frame()                    # Accessing the records of the given lncRNA_Id
-            Entity['Region'] = [re.sub(".*_.*.*_", "", name) for name in Entity.index]      # Get the name of region from the columns of the expression data
-            Entity['Region'] = [re.sub(".bam.out", "", name) for name in Entity['Region'] ]
-            region_order = ['CB', 'STR', 'HIP', 'ACC', 'DPFC', 'VPFC', 'PMC', 'V1C']        # I want to see the boxplot in this specific order
-            Entity['Region'] = pd.Categorical(Entity['Region'], categories=region_order, ordered=True)     # It will infer the categories and their order from the input data
-            Entity['Log2_Expression'] = np.log2(Entity[lncRNA_Id] +1 )
-            
-            import plotly.express as px    # we need this package for plotting
-            plot_title = 'Expression for ' + lncRNA_Id
-            fig = px.box(Entity, x="Region", y="Log2_Expression", color="Region", category_orders={'Region': region_order}, width=850, height=500)
-            fig.update_traces(quartilemethod="exclusive") # or "inclusive", or "linear" by default
-            export = fig.update_layout(legend=dict(orientation="h", yanchor="top", y=-0.15, xanchor="center", x=0.5 ),
-                                        title={'text': plot_title, 'y':0.95, 'x':0.5, 'xanchor': 'center', 'yanchor': 'top'}, 
-                                        yaxis_title= "log2 (Expression)",
-                                        xaxis_title= "Regions",
-                                        legend_title="" )
-            graph= export.to_html()
-            return graph
+REGION_ORDER = ['CB', 'STR', 'HIP', 'ACC', 'DPFC', 'VPFC', 'PMC', 'V1C']  # display order for the boxplot
+EXPRESSION_FILES = {
+    'Human': 'Normalized_Read_Counts_Human.txt',
+    'Chimp': 'Normalized_Read_Counts_Chimp.txt',
+    'Gorilla': 'Normalized_Read_Counts_Gorilla.txt',
+}
 
-        else:
-            graph = "Expression is Not available!"
-            return graph
-    except:
-        print("Not Available!!!")
+
+@lru_cache(maxsize=None)
+def _load_expression_data(organism):
+    # These files are multi-MB; parse each one once per process instead of on every request.
+    file_path = os.path.join(settings.STATIC_DIR, 'files/ExpressionCounts', EXPRESSION_FILES[organism])
+    return pd.read_csv(file_path, sep="\t").set_index('Ids')
+
+
+def Data_preparation(organism, lncRNA_Id):
+    if organism not in EXPRESSION_FILES:
+        return "Expression is Not available!"
+
+    try:
+        Exp_data1 = _load_expression_data(organism)
+        Entity = Exp_data1.loc[lncRNA_Id].to_frame()                    # Accessing the records of the given lncRNA_Id
+        Entity['Region'] = [re.sub(".*_.*.*_", "", name) for name in Entity.index]      # Get the name of region from the columns of the expression data
+        Entity['Region'] = [re.sub(".bam.out", "", name) for name in Entity['Region'] ]
+        Entity['Region'] = pd.Categorical(Entity['Region'], categories=REGION_ORDER, ordered=True)     # It will infer the categories and their order from the input data
+        Entity['Log2_Expression'] = np.log2(Entity[lncRNA_Id] + 1)
+
+        plot_title = 'Expression for ' + lncRNA_Id
+        fig = px.box(Entity, x="Region", y="Log2_Expression", color="Region", category_orders={'Region': REGION_ORDER}, width=850, height=500)
+        fig.update_traces(quartilemethod="exclusive") # or "inclusive", or "linear" by default
+        export = fig.update_layout(legend=dict(orientation="h", yanchor="top", y=-0.15, xanchor="center", x=0.5 ),
+                                    title={'text': plot_title, 'y':0.95, 'x':0.5, 'xanchor': 'center', 'yanchor': 'top'},
+                                    yaxis_title= "log2 (Expression)",
+                                    xaxis_title= "Regions",
+                                    legend_title="" )
+        return export.to_html()
+    except KeyError:
+        return f"Expression is Not available for {lncRNA_Id}"
 
 def Results_from_ids(request, lncIDs, OrgS ):
 
     ORG= OrgS.capitalize()
-    Ids_query = GeneralInfo.objects.all().filter(LncRNA_id__iexact=lncIDs, Organism__iexact=ORG)
-    Organism_dictionary = {"Human":"hg19","Chimp":"panTro4","Gorilla":"gorGor4","Gibbon":"nomLeu3"}
-    ORG_Id= Organism_dictionary[ORG]
+    Ids_query = GeneralInfo.objects.filter(LncRNA_id__iexact=lncIDs, Organism__iexact=ORG)
+    ORG_Id= ORGANISM_ASSEMBLY[ORG]
     ortho = query_processor(Ids_query, ORG )
     Box_Plot = Data_preparation(organism=ORG, lncRNA_Id= lncIDs)
     if isinstance(ortho, list):
@@ -134,67 +130,57 @@ def Results_from_ids(request, lncIDs, OrgS ):
         return render(request, 'warnings.html',{'Warn':ortho})
 
 
+PURE_LNC_FASTA = {
+    'Human': 'Hsapiens_Pure_lncrnas.fasta',
+    'Chimp': 'Chimp_pure_lncRNAs.fasta',
+    'Gorilla': 'Gorilla_pure_lncRNAs.fasta',
+    'Gibbon': 'Gibbon_pure_lncRNAs.fasta',
+}
+
+
 ### Run the blast
 def run_blast(input_file, database_name):
     ## fasta sequences from organisms to consider
-    GenomeFiles = "cat "
-    for db_entity in database_name:
-        if 'Human' in db_entity:
-            path= os.path.join(settings.STATIC_DIR,'files/purelncs','Hsapiens_Pure_lncrnas.fasta')
-            GenomeFiles = GenomeFiles + path + ' '
-        elif 'Chimp' in db_entity:
-            path= os.path.join(settings.STATIC_DIR,'files/purelncs','Chimp_pure_lncRNAs.fasta')
-            GenomeFiles = GenomeFiles + path + ' '
-        elif 'Gorilla' in db_entity:
-            path= os.path.join(settings.STATIC_DIR,'files/purelncs','Gorilla_pure_lncRNAs.fasta')
-            GenomeFiles = GenomeFiles + path + ' '
-        elif 'Gibbon' in db_entity:
-            path= os.path.join(settings.STATIC_DIR,'files/purelncs','Gibbon_pure_lncRNAs.fasta')
-            GenomeFiles = GenomeFiles + path + ' '
-    
+    fasta_paths = [
+        os.path.join(settings.STATIC_DIR, 'files/purelncs', PURE_LNC_FASTA[organism])
+        for organism in PURE_LNC_FASTA
+        if any(organism in db_entity for db_entity in database_name)
+    ]
+
     # All the paths of the files
     Input_genome_pre= os.path.join(settings.STATIC_DIR,'Tmp','GenomePrepInput.fasta')
-    GenomeFiles = GenomeFiles + ">" + Input_genome_pre
+    with open(Input_genome_pre, 'w') as combined_fasta:
+        for path in fasta_paths:
+            with open(path) as fasta_file:
+                combined_fasta.write(fasta_file.read())
 
     blast_path=os.path.join(settings.STATIC_DIR,'softwares/ncbi_blast/bin')
     out_path_db=os.path.join(settings.STATIC_DIR,'Tmp','OrganismDB')
-    
-    # Command for the customized database
-    subprocess.run(GenomeFiles, shell=True)
 
-    makeblastdb_cmd = f"{blast_path}/makeblastdb -in {Input_genome_pre} -dbtype nucl -title organism -out {out_path_db}"
-    
-    # Execute the blastn command
-    subprocess.run(makeblastdb_cmd, shell=True)
+    # Build the customized BLAST database
+    subprocess.run([f"{blast_path}/makeblastdb", '-in', Input_genome_pre, '-dbtype', 'nucl', '-title', 'organism', '-out', out_path_db], check=True)
 
     ## Preapration for Blast
     out_path=os.path.join(settings.STATIC_DIR,'Tmp','BLAST_output.txt')
 
-     # Command to run standalone blastn
-    blastn_cmd = f"{blast_path}/blastn -query {input_file} -db {out_path_db} -out {out_path} -evalue 0.001 -outfmt 6 -perc_identity 95 -max_target_seqs 20"
-
-    # Execute the blastn command
-    subprocess.run(blastn_cmd, shell=True)
-
+    # Run standalone blastn
+    subprocess.run([
+        f"{blast_path}/blastn", '-query', input_file, '-db', out_path_db, '-out', out_path,
+        '-evalue', '0.001', '-outfmt', '6', '-perc_identity', '95', '-max_target_seqs', '20',
+    ], check=True)
 
     if is_file_not_empty(out_path):
         Blast_output = pd.read_csv(out_path,sep='\t', header=None)
         new_headers = ['Query', 'Hit_in_Database', 'Per identity', 'Alignment length', 'Mismatches', 'Gap opens', 'Qurey start', 'Qurey end', 'Hit start', 'Hit end', 'E-value', 'Bit score']
         Blast_output.columns = new_headers
-        #Blast_output['Hit_in_Database'] = Blast_output['Hit_in_Database'].apply(create_hyperlink)
 
-        print(Blast_output)
-        
-        #Blast_output = Blast_output.rename(columns={"Hit_in_Database": "Hit in Database"})  # column renamed
-        Blast_output_subset = Blast_output[['Query', 'Hit_in_Database', 'Per identity', 'Alignment length', 'Mismatches', 'Hit start', 'Hit end', 'E-value', 'Bit score']]
+        Blast_output_subset = Blast_output[['Query', 'Hit_in_Database', 'Per identity', 'Alignment length', 'Mismatches', 'Hit start', 'Hit end', 'E-value', 'Bit score']].copy()
         Blast_output_subset['Hit_in_Database'] = Blast_output_subset['Hit_in_Database'].apply(create_hyperlink)
-        #Blast_output_subset['Hit_in_Database'] = Blast_output_subset.apply(create_hyperlink, axis=1)
 
         Blast_output_subset= Blast_output_subset.to_html(index=False, header=True, classes='table table-bordered', justify='center')
 
         Blast_output_subset = unescape(Blast_output_subset)  ## To get ride of &lt; AND &gt
 
-        print(Blast_output_subset)
         return mark_safe(Blast_output_subset)
     else:
         return('No hit for the given query!')
@@ -227,16 +213,14 @@ def Explore(request):
         if id_search_form.is_valid() and request.POST.get('Idss'):
             IDS = request.POST.get("ID").strip()
             ORG = request.POST.get("Organism").capitalize().strip()
-            Organism_dictionary = {"Human":"hg19","Chimp":"panTro4","Gorilla":"gorGor4","Gibbon":"nomLeu3"}
-            ORG_Id= Organism_dictionary[ORG]
+            ORG_Id= ORGANISM_ASSEMBLY[ORG]
 
             ## Query based on the input IDs AND extracting information.
 
             if IDS and ORG:
-                Ids_query = GeneralInfo.objects.all().filter(LncRNA_id__iexact=IDS, Organism__iexact=ORG )
+                Ids_query = GeneralInfo.objects.filter(LncRNA_id__iexact=IDS, Organism__iexact=ORG )
                 ortho = query_processor(Ids_query, ORG )
                 Box_Plot = Data_preparation(organism=ORG, lncRNA_Id= IDS)
-                print(ortho)
 
                 if isinstance(ortho, list):
                     return render(request, 'results.html', {'Ids_results':Ids_query, 'hsa_ortho':ortho[0], 'pan_ortho':ortho[1], 'gor_ortho':ortho[2], 'gib_ortho':ortho[3], 'graph': Box_Plot, 'browser_org':ORG_Id})
@@ -252,7 +236,6 @@ def Explore(request):
             Position= request.POST.get("Location")
             ORG= request.POST.get("Organism").capitalize().strip()
             Overlap = request.POST.get("Overlap")
-            print (Overlap)
             if Position and ORG:
                 Location_pattern= re.compile(r'(\S+):(\S+)-(\S+)')
                 Location_group = Location_pattern.search(Position.replace(" ",""))
@@ -272,11 +255,10 @@ def Explore(request):
                     q_START2= int(q_START)
                     q_END1 = int(q_END)
 
-                Organism_dictionary = {"Human":"hg19","Chimp":"panTro4","Gorilla":"gorGor4","Gibbon":"nomLeu3"}
-                ORG_Id= Organism_dictionary[ORG]
-                
+                ORG_Id= ORGANISM_ASSEMBLY[ORG]
+
                 ## Query based on the input co-ordinates + overlap if any AND extracting information.
-                Location_query = GeneralInfo.objects.all().filter(Chr__iexact=q_CHR, Start__range=(q_START1, q_START2), End__range=(q_START,q_END1), Organism__iexact=ORG )
+                Location_query = GeneralInfo.objects.filter(Chr__iexact=q_CHR, Start__range=(q_START1, q_START2), End__range=(q_START,q_END1), Organism__iexact=ORG )
                 ortho = query_processor(Location_query, ORG )
                 LncID = ortho[4]
 
@@ -297,7 +279,6 @@ def Explore(request):
             Sequence = request.POST.get("Sequence")
             database_name = seq_search_form.cleaned_data['Organism_db']
             db_names_str = ', '.join(database_name)
-            print(database_name)
             fasta_filename =  os.path.join(settings.STATIC_DIR,'Tmp','Tmp_sequence.fasta')
             with open(fasta_filename, 'w') as fasta_file:
                 fasta_file.write(f'{Sequence}')
@@ -311,7 +292,7 @@ def Explore(request):
         if multipleID_search_form.is_valid() and request.POST.get('MultiIds'):
             Multi_IDs = request.POST.get("MultiIDs")
             Organism = request.POST.get("Organism")
-            Multi_IDs_list = re.split(',|\n|\s',Multi_IDs)
+            Multi_IDs_list = re.split(r',|\n|\s', Multi_IDs)
             Multi_IDs_list = list(filter(lambda x: x != "", Multi_IDs_list))
 
             if Multi_IDs and Organism:
